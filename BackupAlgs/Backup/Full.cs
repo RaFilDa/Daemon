127caaa2dd93cae4ecd4cf0ee2311bbc893600a2
MrazFilip <filda.mraz@volny.cz>
2022-03-10 20:37:46 +0100

    test

--- BackupAlgs/Backup/Full.cs
+++ BackupAlgs/Backup/Full.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using BackupAlgs.Tools;
+using System.Threading;
 
 namespace BackupAlgs.Backup
 {
@@ -16,14 +17,19 @@ namespace BackupAlgs.Backup
         {
             StartLine = startLine;
             Console.SetCursorPosition(0, StartLine);
+            Thread.Sleep(500);
             if (PathTools.PathCheckSource())
             {
                 Console.ForegroundColor = ConsoleColor.Green;
                 Console.WriteLine("OK | Source was accepted");
+
+                Thread.Sleep(500);
+
                 if (PathTools.PathCheckDest())
                 {
                     Console.ForegroundColor = ConsoleColor.Green;
                     Console.WriteLine("OK | Destination was accepted");
+
                     LogTools.AddNewLog("BACKUP: Successful FULL BACKUP");
                 }
                 else
