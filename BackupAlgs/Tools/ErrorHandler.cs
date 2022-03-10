a5c8780e76177e39370734dd94700abc1927891f
radecek04 <radek.piterak@gmail.com>
2022-03-10 22:48:10 +0100

    git fix

---
+++ BackupAlgs/Tools/ErrorHandler.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackupAlgs.Tools
+{
+    public static class ErrorHandler
+    {
+        public static void ThrowError(string backup, string errorMessage)
+        {
+            LogTools.AddNewLog($"ERROR: {backup} BACKUP - {errorMessage}");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("X  | ERROR: " + errorMessage);
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.WriteLine("Press any key to continue");
+            Console.ReadKey(true);
+            Console.Clear();
+        }
+    }
+}
---
+++ BackupAlgs/Windows/LogWindow.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using BackupAlgs.Tools;
+using System.Collections.Generic;
+
+namespace BackupAlgs.Windows
+{
+    public class LogWindow : Window
+    {
+        public Application Parent { get; set; }
+        public string Header { get; set; }
+        public int index { get; set; }
+        public int offset { get; set; }
+        private List<string> ReversedLogs { get; set; }
+        public LogWindow(Application parent, string header)
+        {
+            Console.Clear();
+            Parent = parent;
+            Header = header;
+
+            ReversedLogs = new List<string>(LogTools.Logs);
+            ReversedLogs.Reverse();
+        }
+
+        public override void Draw()
+        {
+            Console.SetCursorPosition(0, 0);
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine(Header);
+            Console.WriteLine("-".PadRight(Console.WindowWidth, '-'));
+
+            for (int i = 0; i < Console.WindowHeight - 3; i++)
+            {
+                Console.WriteLine(" ".PadRight(Console.WindowWidth));
+            }
+            Console.SetCursorPosition(0, 2);
+
+            if(LogTools.Logs.Count > Console.WindowHeight - 5)
+            {
+                for (int i = offset; i < Console.WindowHeight - 5 + offset; i++)
+                {
+                    if(i == index + offset)
+                        Console.ForegroundColor = ConsoleColor.White;
+                    else
+                        Console.ForegroundColor = ConsoleColor.DarkGray;
+                    Console.WriteLine(ReversedLogs[i]);
+                }
+            }
+            else
+                for (int i = 0; i < ReversedLogs.Count; i++)
+                {
+                    if (i == index)
+                        Console.ForegroundColor = ConsoleColor.White;
+                    else
+                        Console.ForegroundColor = ConsoleColor.DarkGray;
+                    Console.WriteLine(ReversedLogs[i]);
+                }
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("-".PadRight(Console.WindowWidth,'-'));
+            Console.WriteLine("Enter - Go back");
+        }
+
+        public override void HandleKey(ConsoleKeyInfo info)
+        {
+            if (info.Key == ConsoleKey.Enter)
+            {
+                Parent.ActiveWindow = new MainWindow(Parent, Header);
+            }
+            else if(info.Key == ConsoleKey.DownArrow)
+            {
+                index++;
+                if (ReversedLogs.Count > Console.WindowHeight - 5)
+                {
+                    if (index == Console.WindowHeight - 5)
+                    {
+                        index--;
+                        offset++;
+                        if (offset + index == ReversedLogs.Count)
+                            offset--;
+                    }
+                }
+                else
+                {
+                    if (index == ReversedLogs.Count)
+                    {
+                        index--;
+                    }
+                }
+            }
+            else if(info.Key== ConsoleKey.UpArrow)
+            {
+                index--;
+                if (index < 0)
+                {
+                    index++;
+                    offset--;
+                    if (offset < 0)
+                        offset++;
+                }
+            }
+        }
+    }
+}
